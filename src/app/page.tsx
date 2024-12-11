f3276157f3cb831a0be3e6e2a8a2b9215f7da352
Sachet Subedi <sachetsubedi001@gmail.com>
2024-12-11 22:04:55 +0545

    fix: board not full visible in biug screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,8 @@ export default function Home() {
   const [gridLines, setGridLines] = useState<T_GridLines>();
   return (
     <div>
-      <Stage width={1000} height={600}>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
-          {/* <Rect x={20} y={20} width={100} height={100} fill="red" draggable /> */}
-          {/* <Rect x={20} y={20} width={100} height={100} fill="green" draggable /> */}
-          {/* <Line stroke={"purple"} strokeWidth={3} points={[0, 0, 100, 100]} /> */}
           {gridLines?.map((line, idx) => {
             return (
               <Line
