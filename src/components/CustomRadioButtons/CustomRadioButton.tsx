d0d58f3c83c363af7fb3cc23bc6c2cb0e8d791a5
Sachet Subedi <sachetsubedi001@gmail.com>
2025-01-04 18:46:13 +0545

    custom radio button changes

--- src/components/CustomRadioButtons/CustomRadioButton.tsx
+++ src/components/CustomRadioButtons/CustomRadioButton.tsx
@@ -3,7 +3,7 @@ import { Dispatch, FC, SetStateAction } from "react";
 const CustomRadioButton: FC<{
   buttons: { label: string; icon: string; value: "tiger" | "goat" }[];
   setValue: Dispatch<SetStateAction<any>>;
-}> = ({ buttons }) => {
+}> = ({ buttons, setValue }) => {
   return (
     <div>
       <div className="flex">
@@ -16,6 +16,7 @@ const CustomRadioButton: FC<{
                 className="mr-2  appearance-none  outline outline-slate-200 checked:outline-3 checked:outline-purple-700  cursor-pointer w-full rounded-md p-2 py-10 px-20"
                 name="customRadioButton"
                 value={button.value}
+                onChange={(e) => setValue(e.target.value)}
               />
               <label
                 className=" absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] flex flex-col items-center cursor-pointer text-black"
